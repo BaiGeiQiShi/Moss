eaa1474b165fb99438778fc285b67862d230b4e3
WhiteKngiht <67951843+BaiGeiQiShi@users.noreply.github.com>
2024-11-04 01:17:53 +0800

    Update GCovBasedMCMCSearch.java

--- singleFile/CovPath/src/java/main/GCovBasedMCMCSearch.java
+++ singleFile/CovPath/src/java/main/GCovBasedMCMCSearch.java
@@ -39,7 +39,7 @@ public class GCovBasedMCMCSearch
         int quan_num = Integer.parseInt(args[11]); //#Total inputs used for path quantification
 	sred_type = Integer.parseInt(args[12]); //Set the size reduction size{0: covered lines; 1: executable bytes; 2: covered stmts (see progcounter's stmt visitor for a stmt's definition).}
 	File base_inputs_file = new File(args[13]);
-	long timeoutDuration;
+	long timeoutDuration = 0;
 	if(args.length == 15){
 	    int timeout = Integer.parseInt(args[14]);
 	    timeoutDuration = TimeUnit.MINUTES.toMillis(timeout);
