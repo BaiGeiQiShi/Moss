11676b21c396298adfb48c1a3bbc6b3a62498ac9
WhiteKngiht <67951843+BaiGeiQiShi@users.noreply.github.com>
2024-12-08 09:41:06 +0800

    Update getLog.py

--- logger/getLog.py
+++ logger/getLog.py
@@ -6,7 +6,7 @@ import subprocess
 logfile,outputfile=argv[1:]
 with open(logfile) as Log, open(outputfile,"w") as Output:
     firstline = Log.readline()
-    if(firstline.startswith("Program name: ")):
+    if(firstline.startswith("Program name: ") or firstline.startswith("Logger Registered.")):
         subprocess.run(["/usr/local/bin/getLog_DomgadFormatted.py",logfile,outputfile])         
     elif(firstline.startswith("Oracle:") or firstline.startswith("Input:") or firstline.startswith("-base")):
             subprocess.run(["/usr/local/bin/getLog_DebopFormatted.py",logfile,outputfile])
